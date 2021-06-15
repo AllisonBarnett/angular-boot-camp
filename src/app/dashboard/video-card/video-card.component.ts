2b93e7c28d7db20bc390c0071ac86611537a1773
Allison <abarnett17@gmail.com>
2021-06-15 13:49:42 -0500

    207 changes

---
+++ src/app/dashboard/video-card/video-card.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Video } from 'src/app/app-types';
+
+@Component({
+  selector: 'app-video-card',
+  templateUrl: './video-card.component.html',
+  styleUrls: ['./video-card.component.css'],
+})
+export class VideoCardComponent {
+  @Input() videos: Video[] = [];
+}
