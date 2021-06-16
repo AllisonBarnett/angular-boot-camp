699770d9dad6ab99874c469033a812f9452a8748
Allison <abarnett17@gmail.com>
2021-06-16 10:21:20 -0500

    add video data service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/dashboard/video-dashboard/video-dashboard.component.ts
+++ src/app/dashboard/video-dashboard/video-dashboard.component.ts
@@ -1,5 +1,14 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Video } from 'src/app/app-types';
+import { HttpClient } from '@angular/common/http';
+
+const apiUrl = 'https://api.angularbootcamp.com/videos';
+
+export interface ViewDetail {
+  age: number;
+  region: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-video-dashboard',
@@ -8,82 +17,17 @@ import { Video } from 'src/app/app-types';
 })
 export class VideoDashboardComponent implements OnInit {
   selectedVideo: Video | undefined;
-
-  // @Input() videos: Video;
-
-  videoList = [
-    {
-      title: 'Angular Observable Data Flow',
-      author: 'Kyle Cordes',
-      id: 'JPuqluYYa-o',
-      viewDetails: [
-        { age: 17, region: 'North America', date: '2016-03-23' },
-        { age: 27, region: 'North America', date: '2016-03-23' },
-        { age: 37, region: 'North America', date: '2016-03-23' },
-        { age: 47, region: 'Europe', date: '2016-03-24' },
-        { age: 37, region: 'North America', date: '2016-03-24' },
-        { age: 17, region: 'North America', date: '2016-03-25' },
-      ],
-    },
-    {
-      title: 'Angular Performance Checklist',
-      author: 'Paul Spears',
-      id: 'cxqRijt9LbQ',
-      viewDetails: [
-        { age: 36, region: 'North America', date: '2016-06-23' },
-        { age: 30, region: 'North America', date: '2016-06-23' },
-        { age: 54, region: 'North America', date: '2016-07-23' },
-        { age: 43, region: 'Europe', date: '2016-0-24' },
-        { age: 32, region: 'North America', date: '2016-08-24' },
-        { age: 32, region: 'North America', date: '2016-08-25' },
-      ],
-    },
-    {
-      title: 'Live App Updates Without The App Store',
-      author: 'Sani Yusuf',
-      id: 's10wrXA-a7Y',
-      viewDetails: [
-        { age: 17, region: 'North America', date: '2016-03-23' },
-        { age: 27, region: 'North America', date: '2016-03-23' },
-        { age: 37, region: 'North America', date: '2016-03-23' },
-        { age: 47, region: 'Europe', date: '2016-03-24' },
-        { age: 37, region: 'North America', date: '2016-03-24' },
-        { age: 17, region: 'North America', date: '2016-03-25' },
-      ],
-    },
-    {
-      title: 'Angular Reactive Forms',
-      author: 'Jack Balbes',
-      id: 'A_Rq6ZsoXpI',
-      viewDetails: [
-        { age: 36, region: 'North America', date: '2016-06-23' },
-        { age: 30, region: 'North America', date: '2016-06-23' },
-        { age: 54, region: 'North America', date: '2016-07-23' },
-        { age: 43, region: 'Europe', date: '2016-0-24' },
-        { age: 32, region: 'North America', date: '2016-08-24' },
-        { age: 32, region: 'North America', date: '2016-08-25' },
-      ],
-    },
-    {
-      title: 'Imperative to Reactive with Angular and RxJS',
-      author: 'John Baur',
-      id: 'VJOPsjlbhdg',
-      viewDetails: [
-        { age: 17, region: 'North America', date: '2016-03-23' },
-        { age: 27, region: 'North America', date: '2016-03-23' },
-        { age: 37, region: 'North America', date: '2016-03-23' },
-        { age: 47, region: 'Europe', date: '2016-03-24' },
-        { age: 37, region: 'North America', date: '2016-03-24' },
-        { age: 17, region: 'North America', date: '2016-03-25' },
-      ],
-    },
-  ];
+  videoList: Video[] = [];
 
   setSelectedVideo(video: Video) {
     this.selectedVideo = video;
   }
 
-  constructor() {}
+  constructor(http: HttpClient) {
+    http
+      .get<Video[]>(apiUrl)
+      .subscribe((videoList) => (this.videoList = videoList));
+  }
 
   ngOnInit(): void {}
 }
